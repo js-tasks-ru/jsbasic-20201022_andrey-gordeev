df6795f726c37396f82496b4f9945575f9cc009d
Andrey Gordeev <a.gordeev@corp.badoo.com>
2020-11-06 22:03:26 +0000

    3-3 sting to camelCase

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,5 +3,13 @@
  * @returns {string}
  */
 function camelize(str) {
-  // ваш код...
+  const splittedString = str.split('');
+  return splittedString.map((item, index, array) => {
+    if (item !== '-') {
+      return item;
+    } else {
+      const nextIndex = index + 1;
+      array[nextIndex] = array[nextIndex].toUpperCase();
+    }
+  }).join('');
 }
